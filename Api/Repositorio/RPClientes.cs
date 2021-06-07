c5b5f2fdf97fa63feed7a72be4ac53872148984c
Luciano2001 <Lucianofrancoloverso2001@gmail.com>
2021-06-06 21:57:45 -0300

    Agregar archivos de proyecto.

---
+++ Api/Repositorio/RPClientes.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using APIREST.Models;
+
+namespace APIREST.Repositorio
+{
+    public class RPClientes
+    {
+        public static List<Cliente> _listaClientes = new List<Cliente>()
+        {
+            new Cliente() { Id = 1, Nombre = "Jorge" , Apellido = "Rodriguez" },
+            new Cliente() { Id = 2, Nombre = "Luciano" , Apellido = "Loverso" },
+            new Cliente() { Id = 3, Nombre = "Vanina" , Apellido = "Gutierrez" }
+        };
+        public IEnumerable<Cliente> ObtenerClientes()
+        {
+            return _listaClientes;
+        }
+
+        public Cliente ObtenerCliente(int id)
+        {
+            var cliente = _listaClientes.Where(cli => cli.Id == id);
+
+            return cliente.FirstOrDefault();
+        }
+
+        public void Agregar(Cliente nuevoCliente)
+        {
+            _listaClientes.Add(nuevoCliente);
+        }
+    }
+}
